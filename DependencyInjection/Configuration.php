6811e90c3e2cb7aee78cfa2a27265e8ecf7a916c
rudak <roiduboul@gmail.com>
2015-05-23 04:34:25 +0200

    add config files

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Rudak\MenuBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link
+ * http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getConfigTreeBuilder()
+	{
+		$treeBuilder = new TreeBuilder();
+		$rootNode    = $treeBuilder->root('rudak_menu');
+		$rootNode->children()
+				 	->arrayNode('items')
+						->prototype('array')
+							->children()
+								->scalarNode('index')->end()
+								->scalarNode('route')->end()
+								->scalarNode('title')->end()
+							->end()
+						->end()
+			  		->end()
+				 ->end();
+
+		return $treeBuilder;
+	}
+}
---
+++ RudakMenuBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rudak\MenuBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RudakMenuBundle extends Bundle
+{
+}
